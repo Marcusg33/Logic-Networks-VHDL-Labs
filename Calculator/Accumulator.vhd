8bab733044d1036041cd396389edc47c6fcd9592
N5co <nicolo.vantini@studenti.unitn.it>
2025-12-02 15:50:11 +0100

    Added architecture to accumulator

--- Calculator/Accumulator.vhd
+++ Calculator/Accumulator.vhd
@@ -8,7 +8,23 @@ entity Accumulator is
         rst         : in  std_logic;
         ac_init        : in  std_logic;
         ac_enable      : in  std_logic;
-        data_in     : in  std_logic_vector(15 downto 0);
-        result_out  : out std_logic_vector(15 downto 0)
+        data_in     : in  signed(15 downto 0);
+        result_out  : out signed(15 downto 0)
     );
 end entity Accumulator;
+
+architecture Behavioral of Accumulator is
+begin
+    process(clk, rst)
+    begin
+        if rst = '0' then
+            result_out <= (others => '0');
+        elsif rising_edge(clk) then
+            if ac_init = '1' then
+                result_out <= (others => '0');
+            elsif ac_enable = '1' then
+                result_out <= data_in;
+            end if;
+        end if;
+    end process;
+end architecture Behavioral;
